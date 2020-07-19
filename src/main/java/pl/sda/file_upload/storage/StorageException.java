31ed2b9dc142ead259c79bf31bb68d059636a648
maniek <maniek@dev.pl>
2020-07-19 08:12:37 +0200

    [init] - initial commit

---
+++ src/main/java/pl/sda/file_upload/storage/StorageException.java
@@ -0,0 +1,12 @@
+package pl.sda.file_upload.storage;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
