01676efff443c394593463aca3b6fed7d0718dbf
maniek <maniek@dev.pl>
2020-07-19 12:57:58 +0200

    [change] - my own upload endpoint in progress..

--- src/main/java/pl/sda/file_upload/MyUploadController.java
+++ src/main/java/pl/sda/file_upload/MyUploadController.java
@@ -1,13 +1,30 @@
 package pl.sda.file_upload;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import pl.sda.file_upload.storage.StorageService;
+
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/my-upload")
+//@RequestMapping("${my-app-upload.prefix}")
 public class MyUploadController {
+
+    private static final Logger log = LoggerFactory.getLogger(MyUploadController.class);
+    private static final String filesKey = "files";
+
+    private final StorageService storageService;
+
+    public MyUploadController(final StorageService storageService) {
+        this.storageService = storageService;
+    }
+
 
     @GetMapping("/show-upload-form")
     public String showMyUploadForm() {
@@ -15,12 +32,23 @@ public class MyUploadController {
     }
 
     @PostMapping("/file-consumer")
-    public String consumeFile() {
-        return "redirect:/";
+    public String consumeFile(@RequestParam("twit")MultipartFile file) {
+        storageService.store(file);
+        return "redirect:/my-upload/all-files";
     }
 
     @GetMapping("/all-files")
-    public String showAllFilesOnServer() {
+    public String showAllFilesOnServer(Model model) {
+        model.addAttribute(filesKey, storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(MyUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toUri().toString())
+                .collect(Collectors.toList()));
         return "all-files";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handler(Exception exception) {
+        log.warn("caugthed exception", exception);
+        return "sorry";
     }
 }
